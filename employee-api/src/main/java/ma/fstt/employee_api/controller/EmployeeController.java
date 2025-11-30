8a593e7627e8796d911789681abad5ef8328a867
Lorraine301 <andriamasylorraine@gmail.com>
2025-11-30 20:32:16 +0100

    Refactor Dockerfile, update application properties, and enhance EmployeeController
    - Simplified Dockerfile for Angular app to run in development mode.
    - Updated MySQL database configuration in application.properties.
    - Improved EmployeeController with additional RESTful methods and imports.
    - Adjusted Angular app's package.json to allow external access.

--- employee-api/src/main/java/ma/fstt/employee_api/controller/EmployeeController.java
+++ employee-api/src/main/java/ma/fstt/employee_api/controller/EmployeeController.java
@@ -1,12 +1,21 @@
 package ma.fstt.employee_api.controller;
 
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+        import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
 import ma.fstt.employee_api.entity.Employee;
 import ma.fstt.employee_api.service.EmployeeService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
-        import java.util.List;
 
 @RestController
 @RequestMapping("/api/employees")
@@ -20,7 +29,7 @@ public class EmployeeController {
     public List<Employee> getAll() {
         return service.getAll();
     }
-
+    
     @GetMapping("/{id}")
     public Employee getById(@PathVariable Long id) {
         return service.getById(id);
